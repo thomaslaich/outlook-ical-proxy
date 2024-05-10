876507c080dcba2570dadd111041f8ac841e756a
Thomas Laich <thomaslaich@gmail.com>
2024-05-10 09:19:26 +0200

    remove keyvault lookup

--- src/functions/OutlookProxy.ts
+++ src/functions/OutlookProxy.ts
@@ -14,21 +14,13 @@ export async function OutlookProxy(
   context.log(`Http function processed request for url "${request.url}"`);
 
   // Initialize Azure Key Vault client
-  const vaultName = "utilities-func-keyvault";
-  const keyVaultUrl = `https://${vaultName}.vault.azure.net`;
-  const credential = new DefaultAzureCredential();
-  const secretClient = new SecretClient(keyVaultUrl, credential);
-
-  // Fetch the Outlook iCal endpoint URL from Key Vault
-  const secretName = "outlook-ical-endpoint";
-  const secret = await secretClient.getSecret(secretName);
-  const outlookICalEndpoint = secret.value;
+  const outlookICalEndpoint = process.env.OUTLOOK_ICAL_ENDPOINT;
 
   const response = await fetch(outlookICalEndpoint);
   const calendarData = await response.text();
   const updatedCalendarData = calendarData
     .replace(/Central Europe Standard Time/g, "Europe/Zurich")
-    .replace(/W. Europe Standard Time/g, "Europe/Zurich");
+    .replace(/W\. Europe Standard Time/g, "Europe/Zurich");
 
   return {
     headers: {
